b199d278d7c56eb931fc0dca49f82724ff28c331
sadrasabouri <sabouri.sadra@gmail.com>
2021-05-07 15:37:41 +0430

    edit : minor edit in pyrandwalk_test.py.

--- pyrandwalk/pyrandwalk_test.py
+++ pyrandwalk/pyrandwalk_test.py
@@ -2,7 +2,7 @@
 """Basic test file."""
 """
 >>> from pyrandwalk import *
->>> import os
+>>> import numpy as np
 >>> states = [0, 1, 2, 3, 4]
 >>> trans = np.array([[1,    0, 0,    0, 0],
 ...                   [0.25, 0, 0.75, 0, 0],
