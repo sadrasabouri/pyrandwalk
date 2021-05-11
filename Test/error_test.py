c65f704817e3c165f6a81db106ce4ba5e7482d02
sadrasabouri <sabouri.sadra@gmail.com>
2021-05-11 21:05:52 +0430

    fix : typo fixed.

--- Test/error_test.py
+++ Test/error_test.py
@@ -10,6 +10,7 @@ pyrandwalk.pyrandwalk_error.pyrandwalkStateError: Invalid type for state paramet
 Traceback (most recent call last):
         ...
 pyrandwalk.pyrandwalk_error.pyrandwalkTransitionsError: Invalid type for transitions parameter. It should be either a list or a numpy array.
+>>> trans = [[1,2,3], [1,2,3], [1,2,3]]
 >>> rw = RandomWalk(['State0', 'State1'], trans)
 Traceback (most recent call last):
         ...
