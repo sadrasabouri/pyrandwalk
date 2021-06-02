5aa6ebaf69c9ac2224edc314029bdb0daa821265
sadrasabouri <sabouri.sadra@gmail.com>
2021-06-03 00:58:46 +0430

    test : tests added accordingly. (#3)

--- pyrandwalk/pyrandwalk_test.py
+++ pyrandwalk/pyrandwalk_test.py
@@ -10,5 +10,14 @@
 ...                   [0, 0, 0.25, 0, 0.75],
 ...                   [0, 0,    0, 1,    0]])
 >>> rw = RandomWalk(states, trans)
-
+>>> states = [0, 1, 2]
+>>> trans = np.array([[1, 0, 0], [1/2, 0, 1/2], [0, 1, 0]])
+>>> rw = RandomWalk(states, trans, payoff=[0, 1, 4])
+>>> rw.best_policy()
+{'continue': [1], 'stop': [0, 2]}
+>>> states = [0, 1, 2]
+>>> trans = np.array([[1, 0, 0], [1/2, 0, 1/2], [0, 1, 0]])
+>>> rw = RandomWalk(states, trans, payoff=[0, 1, 4], cost=[1, 0, 2], discount=0.5)
+>>> rw.best_policy(stop_states=[0])
+{'continue': [], 'stop': [0, 1, 2]}
 """
